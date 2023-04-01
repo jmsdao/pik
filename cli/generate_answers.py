63e58639858586c446c7754e4a9935b6048dcb40
jmsdao <jmsdao@gmail.com>
2023-04-02 02:29:20 +1100

    Add s3 uploading to cli generate_answers

--- cli/generate_answers.py
+++ cli/generate_answers.py
@@ -1,20 +1,24 @@
-import sys
-from time import time
-import shutil
-from pathlib import Path
 import argparse
-import yaml
+import shutil
+import sys
+from io import StringIO
+from pathlib import Path
+from time import time
 
+import boto3
 import pandas as pd
+import yaml
+from dotenv import dotenv_values
 from tqdm import trange
 from transformers import GenerationConfig
 
-from pik.models import load_model_and_tokenizer
-from pik.models.text_generation import TextGenerator
+from pik import ROOT_DIR
 from pik.datasets import load_dataset_and_eval_fn
 from pik.datasets.utils import get_data_ids, get_data_ids_from_file
+from pik.models import load_model_and_tokenizer
+from pik.models.text_generation import TextGenerator
 
-
+SECRETS = dotenv_values(Path(ROOT_DIR, ".env"))
 N_TESTS = 3  # Number of questions to process when estimating
 
 
@@ -92,9 +96,19 @@ def validate_file(file: str) -> None:
             sys.exit()
 
 
-def check_local_paths_exists(filepaths: list[Path]) -> None:
+def check_files_exist_locally(
+    args: argparse.Namespace,
+    config: dict,
+) -> None:
     """Alert user if any local filepaths already exist."""
-    existing_paths = [path for path in filepaths if path.exists()]
+    local_dir = config["results"]["dir"]["local"]
+    output_filepaths = [Path(local_dir) / Path(args.config).name]
+
+    for filename in config["results"]["files"].values():
+        output_filepaths.append(Path(local_dir) / filename)
+
+    existing_paths = [path for path in output_filepaths if path.exists()]
+
     if existing_paths:
         print("Error: The following local filepaths already exist:")
         for path in existing_paths:
@@ -111,14 +125,139 @@ def save_results_locally(
     """Save results locally."""
     local_dir = config["results"]["dir"]["local"]
 
+    # Save config file to disk
     shutil.copy(args.config, Path(local_dir))
+
+    # Save text generations to disk
     text_gens.to_csv(
         Path(local_dir) / config["results"]["files"]["text_generations"], index=False
     )
+
+    # Save QA pairs to disk
     qa_pairs["qid"] = qa_pairs["qid"].astype(int)
     qa_pairs.to_csv(
         Path(local_dir) / config["results"]["files"]["qa_pairs"], index=False
     )
+
+
+def validate_s3_uri(s3_uri: str) -> None:
+    """Validate s3_uri in config file."""
+    if s3_uri:
+        if not s3_uri.startswith("s3://"):
+            print(f'Error: s3_uri "{s3_uri}" must start with "s3://"')
+            sys.exit()
+
+
+def get_s3_bucket_name_and_key(s3_uri: str) -> tuple[str, str]:
+    """Get s3 bucket and key from s3_uri."""
+    if not s3_uri.endswith("/"):
+        s3_uri += "/"
+    bucket_name, s3_key_prefix = s3_uri.replace("s3://", "").split("/", maxsplit=1)
+    return bucket_name, s3_key_prefix
+
+
+def connect_to_s3(bucket_name: str):
+    """Connect to s3."""
+    bucket = boto3.resource(
+        "s3",
+        aws_access_key_id=SECRETS["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=SECRETS["AWS_SECRET_ACCESS_KEY"],
+    ).Bucket(bucket_name)  # type: ignore
+
+    return bucket
+
+
+def check_files_exist_s3(
+    args: argparse.Namespace,
+    config: dict,
+) -> None:
+    """Alert user if any s3 filepaths already exist."""
+    s3_uri = config["results"]["dir"]["s3"]
+    bucket_name, s3_key_prefix = get_s3_bucket_name_and_key(s3_uri)
+    bucket = connect_to_s3(bucket_name)
+
+    output_keys = [s3_key_prefix + Path(args.config).name]
+
+    for filename in config["results"]["files"].values():
+        output_keys.append(s3_key_prefix + filename)
+
+    existing_keys = []
+    try:
+        for obj in bucket.objects.filter(Prefix=s3_key_prefix):
+            if obj.key in output_keys:
+                existing_keys.append(obj.key)
+    except Exception as e:
+        print(e)
+        print(f"Could not read from: {s3_uri}")
+        sys.exit()
+
+    if existing_keys:
+        print("Error: The following s3 filepaths already exist:")
+        for key in existing_keys:
+            print(f"  s3://{bucket_name}/{key}")
+        sys.exit()
+
+
+def check_s3_write_access(
+    args: argparse.Namespace,
+    s3_uri: str,
+) -> None:
+    """Alert user if they don't have write access to s3."""
+    bucket_name, s3_key_prefix = get_s3_bucket_name_and_key(s3_uri)
+    bucket = connect_to_s3(bucket_name)
+    s3_key = s3_key_prefix + Path(args.config).name
+
+    try:
+        bucket.upload_file(args.config, s3_key)
+    except Exception as e:
+        print(e)
+        print(f"Could not write to: {s3_uri}")
+        sys.exit()
+
+
+def save_results_s3(
+    args: argparse.Namespace,
+    config: dict,
+    text_gens: pd.DataFrame,
+    qa_pairs: pd.DataFrame,
+) -> None:
+    """Save results to s3."""
+    s3_uri = config["results"]["dir"]["s3"]
+    bucket_name, s3_key_prefix = get_s3_bucket_name_and_key(s3_uri)
+    bucket = connect_to_s3(bucket_name)
+
+    config_key = s3_key_prefix + Path(args.config).name
+    text_gens_key = s3_key_prefix + config["results"]["files"]["text_generations"]
+    qa_pairs_key = s3_key_prefix + config["results"]["files"]["qa_pairs"]
+
+    # Upload config file
+    try:
+        bucket.upload_file(args.config, config_key)
+    except Exception as e:
+        print(e)
+        print(f"Warning: failed to upload {args.config} to s3 {config_key}")
+        print("Continuing...")
+
+    # Upload text generations from IO buffer
+    try:
+        csv_buffer = StringIO()
+        text_gens.to_csv(csv_buffer)
+        bucket.put_object(Body=csv_buffer.getvalue(), Key=text_gens_key)
+    except Exception as e:
+        print(e)
+        print(f"Warning: failed to upload text generations to s3 {text_gens_key}")
+        print("Continuing...")
+
+    # Upload qa pairs from IO buffer
+    try:
+        csv_buffer = StringIO()
+        qa_pairs["qid"] = qa_pairs["qid"].astype(int)
+        qa_pairs.to_csv(csv_buffer)
+        bucket.put_object(Body=csv_buffer.getvalue(), Key=qa_pairs_key)
+    except Exception as e:
+        print(e)
+        print(f"Warning: failed to upload QA pairs to s3 {qa_pairs_key}")
+        print("Continuing...")
 
 
 if __name__ == "__main__":
@@ -138,21 +277,30 @@ if __name__ == "__main__":
     else:
         print(f"Running experiment using config {args.config}")
 
-    # local_dir checks
-    local_dir = config["results"]["dir"].get("local", None)
-    if local_dir and not args.estimate:
-        validate_local_dir(local_dir)
-        output_filepaths = [Path(local_dir) / Path(args.config).name]
-        for filename in config["results"]["files"].values():
-            output_filepaths.append(Path(local_dir) / filename)
-        check_local_paths_exists(output_filepaths)
-
-    # s3_dir checks # TODO: implement
-
     # Check at least one of local_dir or s3_dir is specified
-    if not local_dir:  # and not s3_dir:
+    if config["results"].get("dir", None) is None:
         print("Error: No local dir or s3 dir specified in config file")
         sys.exit()
+
+    local_dir = config["results"]["dir"].get("local", None)
+    s3_uri = config["results"]["dir"].get("s3", None)
+    if not local_dir and not s3_uri:
+        print("Error: No local dir or s3 dir specified in config file")
+        sys.exit()
+
+    # Local file checks
+    if local_dir and not args.estimate:
+        validate_local_dir(local_dir)
+        if not config["results"]["overwrite"]:
+            check_files_exist_locally(args, config)
+
+    # S3 checks
+    if s3_uri and not args.estimate:
+        if not config["results"]["overwrite"]:
+            check_files_exist_s3(args, config)
+        print(f"Checking write access to {s3_uri}...")
+        check_s3_write_access(args, s3_uri)
+        print("Write access confirmed!")
 
     # Load model and tokenizer
     model_name = config["model"]
@@ -237,6 +385,15 @@ if __name__ == "__main__":
             print(f"  {Path(local_dir) / Path(args.config).name}")
             print("-" * 80)
 
+        if s3_uri:
+            if not s3_uri.endswith("/"):
+                s3_uri += "/"
+            print("Output files to be saved to s3:")
+            for filename in config["results"]["files"].values():
+                print(f"  {s3_uri}{filename}")
+            print(f"  {s3_uri}{Path(args.config).name}")
+            print("-" * 80)
+
         sys.exit()
 
     # Run the experiment
@@ -280,10 +437,15 @@ if __name__ == "__main__":
         if save_frequency and (i + 1) % save_frequency == 0:
             if local_dir:
                 save_results_locally(args, config, text_gens, qa_pairs)
+            if s3_uri:
+                save_results_s3(args, config, text_gens, qa_pairs)
 
     # Save final results
     if local_dir:
         print(f"Saving final results to {local_dir}")
         save_results_locally(args, config, text_gens, qa_pairs)
+    if s3_uri:
+        print(f"Saving final results to {s3_uri}")
+        save_results_s3(args, config, text_gens, qa_pairs)
 
     print("Success!")
