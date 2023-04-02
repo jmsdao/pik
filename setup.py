df49793a84733006a5d355e4929469cd5d4d9bab
jmsdao <jmsdao@gmail.com>
2023-04-02 20:22:40 +1000

    Update env yaml

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="pik",
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     entry_points={
-        'console_scripts': [
-            'gen-answers = cli.generate_answers:main',
+        "console_scripts": [
+            "gen-answers = cli.generate_answers:main",
         ],
-    }
+    },
 )
